1187f4106d36fcf80fb8fc19f5a6620c7be2494b
neolisto <roman.khlevniuk@gmail.com>
2025-08-25 23:58:49 +0200

    fix typo of flower-bracket

--- app_service_plan.tf
+++ app_service_plan.tf
@@ -1,5 +1,5 @@
 resource "azurerm_service_plan" "function_app_sp" {
-  name                = "${var.def_prefix}}-function-app-sp"
+  name                = "${var.def_prefix}-function-app-sp"
   resource_group_name = azurerm_resource_group.function_app_rg.name
   location            = azurerm_resource_group.function_app_rg.location
   os_type             = "Linux"
